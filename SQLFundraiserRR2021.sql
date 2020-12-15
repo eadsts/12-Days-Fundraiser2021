36aff092d5126d3da76f32a0d6dd781764f7cf1c
Tracy Eads <tracyeads1@yahoo.com>
2020-12-15 14:52:00 -0500

    worked on all tables-reference error

--- SQLFundraiserRR2021.sql
+++ SQLFundraiserRR2021.sql
@@ -26,40 +26,39 @@ Email varchar(75) not null
 );
 
 insert Customers (UserName, Password, CustomerFirstName, CustomerLastName, StreetAddress, Apt#, City, State, Zip, PhoneNumber, Email)
-values ('adamsuser','adamspass','Amy','Adams','123 Main Street','1','Hamilton','OH','45013','513-555-1212','amyadams@yahoo.com')
+values ('amy@yahoo.com','amy11','Amy','Adams','123 Main Street','1','Hamilton','OH','45013','513-555-1212','amyadams@yahoo.com')
 
 select *
 	from Customers
 
+create table Sales (
+	Id int not null primary key identity (1,1),
+	CustomerId int not null foreign key references Customers(Id),
+	Quantity int not null DEFAULT 1,
+	Total decimal(11,2) not null,
+	StudentId int foreign key references Students(Id)
+	);
+
+insert Sales (CustomerId, Quantity, Total, StudentId)
+values (1, 2, 20.00, 1)
+
+select * 
+from Sales
+
 create table Tickets (
 	Id int not null primary key identity(1,1),
 	CustomerId int not null foreign key references Customers(Id),
-	TicketBundle varchar(9) not null, /* 1 for 10, 3 for 20, 10 for 40*/
+	SalesId int not null foreign key references Sales(Id),
 	TicketNumber int not null 
 	);
 
-insert Tickets (CustomerId, TicketBundle, TicketNumber)
-values (1, '1 for 10', 1)
+insert Tickets (CustomerId, SalesId, TicketNumber)
+values (1, 1, 1)
 
 select *
 	from Tickets
 
-create table Sales (
-	Id int not null primary key identity (1,1),
-	TicketId int not null foreign key references Tickets(Id),
-	CustomerId int not null foreign key references Customers(Id),
-	TicketBundle varchar(9) not null foreign key references Tickets(TicketBundle),
-	Price decimal(11,2) not null,
-	Quantity int not null DEFAULT 1,
-	Total decimal(11,2) not null,
-	--StudentId varchar(30) foreign key references Students(Id)
-	);
 
-insert Sales (CustomerId, TicketId, TicketBundle, Price, Quantity, Total)
-values (1, 1, '1 for 10', 10.00, 2, 20.00)
-
-select * 
-from Sales
 
 /* We are crediting a student's show choir fees if they sell $200 worth of tickets.
 The student will receive a $100 credit if they sell $200.
@@ -67,21 +66,19 @@ We will ask the customer to select a student name from a drop down menu so that 
 create table Students (
 	Id int not null primary key identity(1,1),
 	GroupName varchar(30) not null,
-	FirstName varchar(30) not null,
-	LastName varchar(30) not null,
-	CustomerId int not null foreign key references Customers(Id),
+	StudentFirstName varchar(30) not null,
+	StudentLastName varchar(30) not null,
 	SalesId int not null foreign key references Sales(Id)
-	
 	);
 
-insert Students (GroupName, FirstName, LastName, CustomerId, SalesId)
+insert Students (GroupName, StudentFirstName, StudentLastName, CustomerId, SalesId)
 values ('Legacy', 'AJ', 'Eads', 1, 1)
 
 select * 
 	from Students
 
 
-select c.CustomerFirstName, c.CustomerLastName, c.PhoneNumber, t.TicketBundle, t.TicketNumber, s.Price, s.Quantity, s.Total, st.GroupName, st.FirstName, st.LastName
+select c.CustomerFirstName, c.CustomerLastName, c.PhoneNumber, c.Email, t.TicketNumber, s.Quantity, s.Total, st.GroupName, st.FirstName, st.LastName
 	from Customers c
 	join Tickets t
 		on t.CustomerId = c.Id
